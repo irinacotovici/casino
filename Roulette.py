8883559cbbdd6e4f2a5e8bba00886ea5862426b7
Igor Custodio João <custodioij@gmail.com>
2017-01-20 10:37:57 +0100

    Created class Table and subclass Roulete; turned global functions into methods for these classes; erased the testing code from Roulette.py; created Simulation.py with codes to test the Roulette.
    Signed-off-by: Igor Custodio João <custodioij@gmail.com>

--- Roulette.py
+++ Roulette.py
@@ -1,31 +1,38 @@
+"""Defines functions related to the roulette game"""
 import random
+
 # This is used to fix the random number generator so we can test the output
 random.seed(3456)
 
-for i in range(1, 6):
-    print(random.randint(1, 10))
-# 7, 8, 5, 3, 9: OK
 
-# Function AboveMinimum
+class Table(object):
+    """Defines a table"""
+    def __init__(self, mini):
+        self.mini = mini
 
+    def above_minimum(self, bets):
+        """ Checks if the bets are above the minimum of the table """
+        above = [bet >= self.mini for bet in bets]
+        return above
 
-def above_minimum(bets, mini):
-    """ Checks if the bets are above the minimum of the table """
-    above = [bet >= mini for bet in bets]
-    return above
+class Roulette(Table):
+    """Defines a roulette table"""
+    def __init__(self, mini):
+        Table.__init__(self, mini)
 
-print(above_minimum([1, 2, 4, 5], 2))
-# test OK
+    def spin_the_wheel(self, bet_on):
+        """Spins the wheel and tells which players won (not accounting for minimum bet)"""
+        draw = random.randint(0, 36)
+        wins = [bet == draw for bet in bet_on]
+        if not any(wins):
+            print("No player won")
+        print("Ball lands on " + str(draw))
+        return wins
 
-
-def spin_the_wheel(bets):
-    draw = random.randint(0, 36)
-    wins = [bet == draw for bet in bets]
-    if not any(wins):
-        print("No player won")
-    print(draw)
-    return wins
-
-print(spin_the_wheel([2, 8, 9, 26, 14]))
-print(spin_the_wheel([2, 8, 9, 26, 14]))
-# Tests OK
+    def simulate_game(self, bets, bet_on):
+        """Simulates a round of roulette, gives the amounts won by each player and the casino's profit"""
+        above_list = self.above_minimum(bets)
+        wins = self.spin_the_wheel(bet_on)
+        value_won = [above * win * bet * 30 for win, bet, above in zip(wins, bets, above_list)]
+        profit = sum(bets) - sum(value_won)
+        return [value_won, profit]
---
+++ Simulation.py
@@ -0,0 +1,7 @@
+import Roulette
+
+amounts1 = [10, 85, 120, 65, 150, 122]
+bets1 = [10, 24, 36, 0, 11, 24]
+table1 = Roulette.Roulette(100)
+print(table1.simulate_game(amounts1, bets1))
+print(table1.simulate_game(amounts1, bets1))
